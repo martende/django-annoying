f8c2f55d46a2c8bb68a19e2cbadc4687e48a1227
martende <entcrd@gmail.com>
2013-04-29 15:15:29 +0200

    django 1.6 support (only for 1.6)

--- annoying/decorators.py
+++ annoying/decorators.py
@@ -143,7 +143,7 @@ class JsonResponse(HttpResponse):
     HttpResponse descendant, which return response with ``application/json`` mimetype.
     """
     def __init__(self, data):
-        super(JsonResponse, self).__init__(content=simplejson.dumps(data), mimetype='application/json')
+        super(JsonResponse, self).__init__(content=simplejson.dumps(data), content_type='application/json')
 
 
 
